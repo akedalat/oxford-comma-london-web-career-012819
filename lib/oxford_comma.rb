a53ad010503ddd26463c92831e00e801bcc5e92e
Kian Edalat <akedalat@gmail.com>
2019-01-09 22:34:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,16 @@
 def oxford_comma(array)
+
 if array.size == 1
 array.join(",")
+
 elsif array.size === 2
 array.join(" and ")
+
+elsif array.size == 3
+  third_element = array[2]
+  array.pop
+  array.push("and #{third_element}")
+  
+  
 end
 end
